d67ce4719f555f0ddea78d8308d3f3457f4b97c8
mifloga <mifloga@gmail.com>
2026-06-23 19:41:04 +0200

    Update doors.js

--- doors.js
+++ doors.js
@@ -8,6 +8,55 @@ const doors = {
 	
 	4: door04,
 	
-	5: door05
+	5: door05,
+	
+    6: door06,
 
+    7: door07,
+	
+	8: door08,
+	
+	9: door09,
+	
+	10: door10,
+	
+	11: door11,
+
+    12: door12,
+	
+	13: door13,
+	
+	14: door14,
+	
+	15: door15,
+	
+	16: door16,
+
+    17: door17,
+	
+	18: door18,
+	
+	19: door19,
+	
+	20: door20,
+	
+	21: door21,
+
+    22: door22,
+	
+	23: door23,
+	
+	24: door24,
+	
+	25: door25,
+	
+	26: door26,
+
+    27: door27,
+	
+	28: door28,
+	
+	29: door29,
+	
+	30: door30
 };
